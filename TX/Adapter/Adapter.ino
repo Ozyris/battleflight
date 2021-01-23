dc3cee652adaf7179ad826953e83e8127380d49f
Ozyris <ozyris@bk.ru>
2021-01-23 18:49:18 +0300

    new packet type

--- TX/Adapter/Adapter.ino
+++ TX/Adapter/Adapter.ino
@@ -4,7 +4,6 @@
 #include "FrSkySportDecoder.h"
 #include "SoftwareSerial.h"
 
-
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #ifndef STASSID
@@ -12,7 +11,13 @@
 #define STAPSK  "123456789"
 #endif
 
-String playerChar = "P1RedQuad0";
+//uint8_t Plr, Col, Typ, Wp;
+uint8_t Plr = 0x01;       //Player1
+uint8_t Col = 0xB1;       //Colour - Red
+uint8_t Typ = 0xD1;       //Type - Quad
+uint8_t Wp  = 0xE1;       //Weapon - No
+float int_arr[2];         //Lat+Lon
+
 const char * udpAddress = "192.168.4.1";
 const int udpPort = 4210;
 WiFiUDP Udp;
@@ -26,7 +31,7 @@ uint16_t decodeResult;
 void setup()
 {
   // Configure the decoder serial port and sensors (remember to use & to specify a pointer to sensor)
-  decoder.begin(FrSkySportSingleWireSerial::SOFT_SERIAL_PIN_D1, &gps);
+  decoder.begin(FrSkySportSingleWireSerial::SOFT_SERIAL_PIN_D2, &gps);
 
   Serial.begin(115200);
   
@@ -41,18 +46,39 @@ void setup()
 
 void loop()
 {
-  decodeResult = decoder.decode(); //SPort decode
+  decodeResult = decoder.decode();
 
   // Display data once a second to not interfeere with data decoding
   currentTime = millis();
   if(currentTime > displayTime)
   {
     displayTime = currentTime + 1000;
-    
-    // Get and send GPS sensor data
-    String msg = playerChar + "$n"+String(gps.getLat(), 6) + "$e"+String(gps.getLon(), 6)+"$s";
+
+    Serial.println("");
+
+    int_arr[0] = gps.getLat();
+    int_arr[1] = gps.getLon();
+
+    uint8_t i,j;  
+    uint8_t to_send_arr[8]; // 2 x (32/8)
+    float *p;
+    uint8_t *p2;
+
+    p2=(uint8_t *)int_arr;
+
+    for (i=0;i<8;i++)
+    {
+      to_send_arr[i] = p2[i];
+    }
+
     Udp.beginPacket(udpAddress,udpPort);
-    Udp.write(msg.c_str()); 
+
+    for (j=0;j<8;j++)
+    {
+    Udp.write(to_send_arr[j]);
+    }
+    Udp.write(Plr);    Udp.write(Col);    Udp.write(Typ);    Udp.write(Wp);
     Udp.endPacket();
+    Serial.println("");
   }
 }
